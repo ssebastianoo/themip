f2267eee7d7643e05f155ab1695d18f58d54d178
ssebastianoo <sebastianogirotto05@gmail.com>
2022-01-26 16:15:33 +0100

    Updates

---
+++ precache-manifest.a11ee64683c7ac172f0af7092dd769b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3192291edba9dca390bf43160d8721",
+    "url": "/themip/index.html"
+  },
+  {
+    "revision": "e8a0ba3673e0221f9127",
+    "url": "/themip/static/css/main.f97bd818.chunk.css"
+  },
+  {
+    "revision": "db1cb8df0f5ef86834f4",
+    "url": "/themip/static/js/2.2eafb09c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/themip/static/js/2.2eafb09c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a0ba3673e0221f9127",
+    "url": "/themip/static/js/main.32b73c4d.chunk.js"
+  },
+  {
+    "revision": "967adf34803127e56ab3",
+    "url": "/themip/static/js/runtime-main.5d9c8a9a.js"
+  }
+]);
